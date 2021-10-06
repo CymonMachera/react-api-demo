0533deaa4b95fb5a18a4e60361831d69eab178a2
cymonmachera <cymonmachera1@gmail.com>
2021-10-06 21:18:45 +0300

    Added get API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+const [collectionList, setCollectionList] = useState([]);
+
+useEffect(() => {
+  requestCollections();
+}, [setCollectionList])
+
+async function requestCollections(){
+  const res = await fetch(`http://localhost:3100/collection`);
+  const json = await res.json();
+  setCollectionList(json)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Collection Table</h1>
+  <table bordered hover responsive="sm">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Collect ID</th>
+              <th>Farmer UIN</th>
+              <th>Pond ID</th>
+              <th>Procedure</th>
+              <th>Group</th>
+              <th>Amount</th>
+              <th>Driver Name</th>
+              <th>Truck Plate No</th>
+              <th>Dep Time</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {collectionList.map(c => (
+              <tr key={c.id}>
+                <td>{c.date}</td>
+                <td>{c.collect_id}</td>
+                <td>{c.farmer_uin}</td>
+                <td>
+                  {c.pond_id}</td>
+                <td>{c.procedure}</td>
+                <td>{c.group}</td>
+                <td>{c.amount}</td>
+                <td>{c.driver_name}</td>
+                <td>{c.truck_plate_no}</td>
+                <td>{c.dep_time}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
     </div>
   );
 }
